d76dc6843b012556f7d97d231e46ba606b1ea75e
gokul18 <gokulakrishnan.doodleblue@gmail.com>
2024-07-28 21:19:10 +0530

    update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -341,8 +341,12 @@ const Home: React.FC = () => {
               <div className={"palyers-match-card"} key={index}
                 style={{ backgroundImage: `url(${court})` }}>
 
-                {searchloading ?
-                  <ScaningLoading /> :
+                {!searchloading ?
+                  <div className='w-100 d-flex align-items-center justify-content-center mt-3'>
+
+                    <ScaningLoading />
+                  </div>
+                  :
                   <div>
 
                     {Boolean(match.start) ? match.players.slice(0, 2).map((x: any, index: any) => (
